7fc171bd4294090f211f2c981faa37feba5236d6
Ernesto <ernestosadeghi@gmail.com>
2025-06-26 17:17:26 +0330

    adding js module to project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,5 +5,7 @@ export default defineNuxtConfig({
   ,  css: [
     'bootstrap/dist/css/bootstrap.min.css','bootstrap-icons/font/bootstrap-icons.css'
   ]
-
+  ,plugins: [
+    { src: '~/plugins/bootstrap.client.js', mode: 'client' }
+  ]
 })
---
+++ plugins/bootstrap.client.js
@@ -0,0 +1,6 @@
+import { defineNuxtPlugin } from '#app'
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.min'
+
+export default defineNuxtPlugin(nuxtApp => {
+  nuxtApp.provide('bootstrap', bootstrap)
+})
