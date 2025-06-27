f14df251905194e4ef17ba542d887cd51c0cb0fc
Ernesto <ernestosadeghi@gmail.com>
2025-06-27 22:53:08 +0330

    creating category page

---
+++ pages/products/categories-[cat].vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <h1 class="text-center text-primary ">
+      {{ $route.params.cat }}
+
+    </h1>
+    <div class="row g-5 my-5">
+
+      <Product v-for="product in products" :key="product.id" :product="product"></Product>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+const route = useRoute()
+
+// const url = computed(() => `https://fakestoreapi.com/products/category/${route.params.cat}`)
+// console.log(url);
+
+const products = ref([])
+
+watchEffect(async () => {
+  const url = `https://fakestoreapi.com/products/category/${route.params.cat}`
+  products.value = await $fetch(url)
+})
+
+
+
+
+
+</script>
+
+<style>
+
+</style>
